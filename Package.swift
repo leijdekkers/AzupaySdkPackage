f1877ffbb0d5fbf4fd3d2e1c06922444d2eee9fa
Peter leijdekkers <leijdekkers2010@gmail.com>
2021-05-12 22:12:52 +1000

    added alamofire as dependency

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// https://stackoverflow.com/questions/65220359/add-package-dependency-for-a-binary-target-with-swift-package-manager
 import PackageDescription
 
 let package = Package(
@@ -16,17 +17,20 @@ let package = Package(
   ],
     
     dependencies: [
-        .package(name: "PhoneNumberKit", url: "https://github.com/marmelroy/PhoneNumberKit.git", from: "3.3.3")
+        .package(name: "PhoneNumberKit", url: "https://github.com/marmelroy/PhoneNumberKit.git", from: "3.3.3"),
+        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0"))
     ],
     
   targets: [
     .binaryTarget(name: "AzupaySdkBT",path: "./Sources/AzupaySdk.xcframework"),
     .target(name: "PhoneNumberKitBT", dependencies: ["PhoneNumberKit"]),
+    .target(name: "AlamofireBT", dependencies: ["Alamofire"]),
     
         .target(name: "AzupaySdkTargets",
                        dependencies: [
                            .target(name: "AzupaySdkBT", condition: .when(platforms: .some([.iOS]))),
-                           .target(name: "PhoneNumberKitBT", condition: .when(platforms: .some([.iOS])))
+                           .target(name: "PhoneNumberKitBT", condition: .when(platforms: .some([.iOS]))),
+                           .target(name: "AlamofireBT", condition: .when(platforms: .some([.iOS])))
                        ],
                        path: "./Sources/AzupaySdkTargets"
                )
