c138856ed296d4fce779bfabfc6bb402ccf78f31
Peter leijdekkers <leijdekkers2010@gmail.com>
2021-05-13 11:23:49 +1000

    Removed RxCocoa

--- Package.swift
+++ Package.swift
@@ -18,24 +18,19 @@ let package = Package(
     
     dependencies: [
         .package(name: "PhoneNumberKit", url: "https://github.com/marmelroy/PhoneNumberKit.git", from: "3.3.3"),
-        .package(name: "RxAlamofire", url: "https://github.com/RxSwiftCommunity/RxAlamofire.git", from: "6.1.0"),
-        .package(name: "RxSwift", url: "https://github.com/ReactiveX/RxSwift.git", from: "6.1.0"),
+        .package(name: "RxAlamofire", url: "https://github.com/RxSwiftCommunity/RxAlamofire.git", from: "6.1.0")
     ],
     
   targets: [
     .binaryTarget(name: "AzupaySdkBT",path: "./Sources/AzupaySdk.xcframework"),
-    .binaryTarget(name: "RxCocoaBT",path: "./Sources/RxCocoa.xcframework"),
     .target(name: "PhoneNumberKitBT", dependencies: ["PhoneNumberKit"]),
-    .target(name: "AlamofireBT", dependencies: ["RxAlamofire"]),
-    .target(name: "RxSwiftBT", dependencies: ["RxSwift"]),
+    .target(name: "RxAlamofireBT", dependencies: ["RxAlamofire"]),
         
         .target(name: "AzupaySdkTargets",
                        dependencies: [
                            .target(name: "AzupaySdkBT", condition: .when(platforms: .some([.iOS]))),
-                           .target(name: "RxCocoaBT", condition: .when(platforms: .some([.iOS]))),
                            .target(name: "PhoneNumberKitBT", condition: .when(platforms: .some([.iOS]))),
-                           .target(name: "AlamofireBT", condition: .when(platforms: .some([.iOS]))),
-                           .target(name: "RxSwiftBT", condition: .when(platforms: .some([.iOS])))
+                           .target(name: "RxAlamofireBT", condition: .when(platforms: .some([.iOS]))),
                        ],
                        path: "./Sources/AzupaySdkTargets"
                )
