f35883247a87acef15e4e0c2df6e206e8e53ff14
Peter leijdekkers <leijdekkers2010@gmail.com>
2021-05-13 13:54:16 +1000

    Reworked to Alamofire no more RxSwift etc

--- Package.swift
+++ Package.swift
@@ -18,19 +18,19 @@ let package = Package(
     
     dependencies: [
         .package(name: "PhoneNumberKit", url: "https://github.com/marmelroy/PhoneNumberKit.git", from: "3.3.3"),
-        .package(name: "RxAlamofire", url: "https://github.com/RxSwiftCommunity/RxAlamofire.git", from: "6.1.0")
+        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", from: "5.2.0")
     ],
     
   targets: [
     .binaryTarget(name: "AzupaySdkBT",path: "./Sources/AzupaySdk.xcframework"),
     .target(name: "PhoneNumberKitBT", dependencies: ["PhoneNumberKit"]),
-    .target(name: "RxAlamofireBT", dependencies: ["RxAlamofire"]),
+    .target(name: "AlamofireBT", dependencies: ["Alamofire"]),
         
         .target(name: "AzupaySdkTargets",
                        dependencies: [
                            .target(name: "AzupaySdkBT", condition: .when(platforms: .some([.iOS]))),
                            .target(name: "PhoneNumberKitBT", condition: .when(platforms: .some([.iOS]))),
-                           .target(name: "RxAlamofireBT", condition: .when(platforms: .some([.iOS]))),
+                           .target(name: "AlamofireBT", condition: .when(platforms: .some([.iOS]))),
                        ],
                        path: "./Sources/AzupaySdkTargets"
                )
